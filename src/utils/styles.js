4ca1d7ad764c48449fbbef1c8cea9365e8d3b1c8
Steve Helms <steve@cadenrock.com>
2021-01-03 09:14:43 -0600

    Added loader component

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
 
+export const colors = {
+    primaryOrange: '#e25a2d',
+}
+
 export const Container = styled.div`
     border: 1px solid #aeaeae;
     border-radius: 3px;
